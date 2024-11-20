883cef015a233e707622803163cf713742d3edea
Sarubala-B <poojasaru1112000@gmail.com>
2024-11-20 13:12:51 +0530

    Create 20241108083351-create-todo-table.js

---
+++ Todo-node/migrations/20241108083351-create-todo-table.js
@@ -0,0 +1,42 @@
+// migrations/TIMESTAMP-create-todos-table.js
+'use strict';
+
+const sequelize = require("../config/database");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ToDos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      taskName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      deleteFlag: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ToDos');
+  },
+};
+
