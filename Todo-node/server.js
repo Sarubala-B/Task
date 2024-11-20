bffa20e350f909173d356b5aafe36b9d41d0d2e7
Sarubala-B <poojasaru1112000@gmail.com>
2024-11-20 14:29:00 +0530

    Create server.js

---
+++ Todo-node/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('./config/database');
+const userRoutes = require('./routes/signup');
+const todoRoutes = require('./routes/todo');
+const loginRoutes = require('./routes/login');
+const app = express();
+const crypto = require('crypto');
+
+// Middleware for CORS
+app.use(cors({
+  origin: 'http://localhost:3000', // Your React frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'userid'],
+}));
+
+
+// Middleware for parsing JSON
+app.use(express.json());
+
+// Define the routes
+app.use('/api', userRoutes);
+app.use('/api', todoRoutes);
+app.use('/api',loginRoutes);
+
+// Set up the server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
