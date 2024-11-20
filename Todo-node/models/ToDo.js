1c7f7682b6d693c50b63814aa7dc3b2cc082681c
Sarubala-B <poojasaru1112000@gmail.com>
2024-11-20 14:19:13 +0530

    Create ToDo.js

---
+++ Todo-node/models/ToDo.js
@@ -0,0 +1,33 @@
+// models/ToDo.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database'); // Adjust this import to your config path
+
+const ToDo = sequelize.define('ToDos', {
+ id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  taskName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.INTEGER,
+    default: 0,
+    // allowNull: false
+  },
+  deleteFlag: {
+    type: DataTypes.INTEGER,
+    default: 0,
+    // allowNull: false
+  }
+},
+{
+    timestamps: true, // Enables createdAt and updatedAt
+    tableName: 'ToDos', // Ensures Sequelize matches table created in migration
+}
+);
+
+module.exports = ToDo;
