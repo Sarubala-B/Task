0a8604ae88f11d2c074f9771ed13a887f0a22e4a
Sarubala-B <poojasaru1112000@gmail.com>
2024-12-24 12:56:51 +0530

    Update server.js

--- Todo-node/server.js
+++ Todo-node/server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const sequelize = require('./config/database');
 const userRoutes = require('./routes/signup');
 const todoRoutes = require('./routes/todo');
 const loginRoutes = require('./routes/login');
 const app = express();
-const crypto = require('crypto');
 
 // Middleware for CORS
 app.use(cors({
-  origin: 'http://localhost:3000', // Your React frontend URL
+  origin: 'http://localhost:3000', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'userid'],
 }));
@@ -21,7 +19,7 @@ app.use(express.json());
 // Define the routes
 app.use('/api', userRoutes);
 app.use('/api', todoRoutes);
-app.use('/api',loginRoutes);
+app.use('/api', loginRoutes);
 
 // Set up the server
 const PORT = 5000;
